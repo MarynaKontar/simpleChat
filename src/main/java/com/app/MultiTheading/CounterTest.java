b3433d7b5c6be369778aa86ba7e591d54a95645f
Maryna Kontar <marina.acoustic@gmail.com>
2017-07-17 17:31:43 +0300

    multithreading CounterTest fix

--- src/main/java/com/app/MultiTheading/CounterTest.java
+++ src/main/java/com/app/MultiTheading/CounterTest.java
@@ -1,29 +1,52 @@
 package com.app.MultiTheading;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by User on 03.07.2017.
  */
 public class CounterTest {
-    
-    private static class Counter{
-        private int i;
-        private Object mu = new Object();
-        private AtomicInteger c = new AtomicInteger();// !!!!!ОБЕСПЕЧИВАЕТ ПОТОКОБЕЗОПАСНОСТЬ
 
-        private int getAndIncrement(){
-//            // критическая секция (окружена synchronized) = монитор. Пока эту секцию не выполнят, заново нельзя зайти другому thread
-//            synchronized (this) {//блок по экземпляру текущего класса synchronize. Можно synchronized (this) или synchronized (mu)
-////            int tmp = i;
-////            i++;
-////            return tmp;
-//            return i++;
+    private static class Counter {
+        private int i;
+        private Object mu = new Object(); //mutex - отдельный объект, который используется только для синхронизации
+
+        //в java.util.concurrent.atomic есть Atomic primitives (AtomicInteger, AtomicLong,...), команды которых
+        // (getAndIncrement(), например) гарантируют атомарность
+        private AtomicInteger c = new AtomicInteger();// !!!!!ОБЕСПЕЧИВАЕТ ПОТОКОБЕЗОПАСНОСТЬ использовать вместо і++
+        private AtomicLong cLong = new AtomicLong();// !!!!!ОБЕСПЕЧИВАЕТ ПОТОКОБЕЗОПАСНОСТЬ использовать сместо і++
+
+
+        //
+        private long getAndIncrement() {
+//            // критическая секция (окружена synchronized) = монитор(в эту секцию,
+//            //в один момент времени, захватывая монитор может зайти только один поток (thread)).
+//            //Пока эту секцию не выполнят, заново нельзя зайти другому thread
+//            synchronized (this) {//synchronized блок по экземпляру текущего класса
+//            //(т.е. делаем потокобезопасным по объекту и монитору (в скобках - this=экземпляр класса Counter)).
+//            //Можно synchronized (this) или synchronized (mu)- mutex. Ничем эти два способа не отличается.
+            //synchronized (this) равноценно синхронизации по методу. Тю ею когда пишем synchronized метод,
+            // это то же самое,что synchronized (this).
+            // Т.е. когда поток заходит в synchronized метод, он видит и др. synchronized методы этого класса.
+            // Свойство, когда один видет результаты другого справедливо только
+//            когда они на ОДНОМ мониторе.
+            // Главное, чтобы синхронизация была по одному и тому же монитору.
+
+//            long tmp = i;
+//            i++;
+//            return tmp;
+            return i++;
 //        }
-            return c.getAndIncrement();
+//            return c.getAndIncrement();
         }
-        private int get(){
-            return i;
+
+        private long get() {
+            synchronized (this) {
+//            synchronized (mu){
+                return i;
+//                return c.get();
+            }
         }
     }
 
